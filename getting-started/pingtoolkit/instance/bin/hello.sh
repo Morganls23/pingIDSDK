7548fd7df5940948ac0bf9b8f64f46ad0e84630a
Morgan <morgan.smith118@gmail.com>
2020-03-11 14:16:34 -0600

    add some extra stuff

---
+++ getting-started/pingtoolkit/instance/bin/hello.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+echo "
+##########################################
+# Hello from the Ping Tool Kit Container!
+#
+#     Date: $(date)
+# Hostname: $(hostname)
+##########################################
+"
